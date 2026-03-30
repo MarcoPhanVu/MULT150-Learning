5e65b44cd97d9a0bace6d0494652651d9cbf2816
Phan Vũ Tuấn Anh <88922348+MarcoPhanVu@users.noreply.github.com>
2026-03-30 00:22:08 -0700

    update H8

---
+++ bPhan_Hour8_P1/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Mouse X");
+        float moveY = Input.GetAxis("Mouse Y");
+
+        transform.Translate(moveX * moveSpeed,
+                            moveY * moveSpeed,
+                            0f);
+    }
+}
--- bPhan_Hour8_P1/Assets/Scripts/CubeScript.cs
+++ bPhan_Hour8_P1/Assets/Scripts/CubeScript.cs
@@ -13,6 +13,8 @@ public class CubeScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(.05f, 0f, 0f);
+        transform.Rotate(0f, 0f, 1f);
+        transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
     }
 }
