ab8ffacb19c8916e0808d48dbab9f6144e4c8a50
Phan Vũ Tuấn Anh <88922348+MarcoPhanVu@users.noreply.github.com>
2026-03-29 23:55:48 -0700

    H8 update

---
+++ bPhan_Hour8_P1/Assets/SideQuest.cs
@@ -0,0 +1,4 @@
+/*TakeDamage*/
+/*int*/
+/*int damageAmount, just this one*/
+/*creating a new health every time the function is called, and then subtracting the damage amount from it, and then printing out the new health value. Downside: health is reset every time so it'll depends on the codder's usage*/
