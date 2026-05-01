56d065b1a735514605452cbc602f67bbd96fef0c
Phan Vũ Tuấn Anh <88922348+MarcoPhanVu@users.noreply.github.com>
2026-04-30 23:12:29 -0700

    fix H10 again

---
+++ bPhan_Hour10/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,11 @@
+// The game is quite easy for now
+// Adding a timer will make it a bit mor challenging
+// Maybe add an angry ball to "kill" the player if they take too long?
+// wining the game is fun and having to chase the ball is tedious.
+
+// Additional changes
+// Add a restart menu when the game is over
+// Add a timer to make the game more challenging
+// End the game if the player takes too long
+// Adjusted the size of bumber
+// Changed the shape of the bumper
--- bPhan_Hour10/Assets/Scripts/GameManager.cs
+++ bPhan_Hour10/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,21 +9,48 @@ public class GameManager : MonoBehaviour
     public float startSpeed = 8f;
     public GoalScript blue, green, red, orange;
     private bool isGameOver = true;
+	private float elapsedTime = 0;
+
+	private void StartGame()
+	{
+        blue.isSolved = false;
+        green.isSolved = false;
+        red.isSolved = false;
+        orange.isSolved = false;
+        isGameOver = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
 
     // Update is called once per frame
       void Update ()
     {
         // If all four goals are solved then the game is over
-       isGameOver = blue.isSolved && green.isSolved && red.isSolved &&  orange.isSolved;
+        // Debug.Log("Checking if game is over...");
+        isGameOver = blue.isSolved && green.isSolved && red.isSolved && orange.isSolved;
+        // if(Input.GetKeyDown(KeyCode.Return))
+        // {
+        //     Debug.Log("Enter check...");
+        // }
     }
     void OnGUI()
         {
+            // Debug.Log("Checking if game is over in OnGUI...");  
         if(isGameOver)
         {
             Rect rect = new Rect (Screen.width / 2 - 100, Screen.height /  2 - 50, 200, 75);
             GUI.Box (rect, "Game Over");
             Rect rect2 = new Rect (Screen.width / 2 - 30, Screen.height /  2 - 25, 60, 50);
             GUI.Label (rect2, "Good Job!");
+
+            Rect restartButton = new Rect(Screen.width/2 - 120, Screen.height/2, 240, 30);
+            string message = "Press Enter to replay";
+
+            if (GUI.Button(restartButton, message) || Input.GetKeyDown(KeyCode.Return))
+			{
+                Debug.Log("Restarting Game...");
+                StartGame();
+				// start the game if the user clicks to play
+			}
             }
         }
     }
