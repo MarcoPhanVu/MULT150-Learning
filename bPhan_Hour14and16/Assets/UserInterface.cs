15e5c284161a084290c83a509b5fe497a1e7cc96
Phan Vũ Tuấn Anh <88922348+MarcoPhanVu@users.noreply.github.com>
2026-05-17 21:14:44 -0700

    H16 with integrated buttons to move scenes

--- bPhan_Hour14and16/Assets/UserInterface.cs
+++ bPhan_Hour14and16/Assets/UserInterface.cs
@@ -1,6 +1,7 @@
-// Modifications that I made:
-// Background image
-// Font size, color, styles
-// Layout of UI elements
-// Title and a small description
-// Button functionality
+/* Modifications that I made:
+Background image
+Font size, color, styles
+Layout of UI elements
+Title and a small description
+Button functionality
+*/
