20b14f88821b2220f5164a51d2544a2c0c981f57
Phan Vũ Tuấn Anh <88922348+MarcoPhanVu@users.noreply.github.com>
2026-05-18 18:51:37 -0700

    fix animation H17

--- bPhan_Hour17and18/Assets/Scripts/AnimController.cs
+++ bPhan_Hour17and18/Assets/Scripts/AnimController.cs
@@ -2,96 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PrefabGenerator : MonoBehaviour
+public class AnimController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public GameObject lampPrefab;
-    public Vector3 spawningRange, spawningPosition;
-
+    Animator animator;
     void Start()
     {
-        // Randomly spawn 5 lamps within the specified range
-        // for (int i = 0; i < 5; i++)
-        // {
-        //     SpawnLampRandom();
-        // }
-        
-        // Spawn 5 consecutive lamps in a line
-        // Vector3 linePosition = spawningPosition;
-        // for (int i = 0; i < 10; i++)
-        // {
-        //     if (i == 5)
-        //     {
-        //         linePosition = new Vector3(spawningPosition.x, spawningPosition.y, spawningPosition.z + 12f); // Reset position for the next(to) line
-        //     }
-        //     SpawnLampInLine(linePosition, 0); // 0 == x
-        //     linePosition += new Vector3(10f, 0f, 0f);
-        // }
+        animator = GetComponent<Animator>();
     }
 
-    void SpawnLampRandom()
-    {
-        Vector3 randomPosition = new Vector3(
-            Random.Range(-spawningRange.x, spawningRange.x),
-            1f,
-            Random.Range(-spawningRange.z, spawningRange.z)
-        );
-
-        Instantiate(lampPrefab, spawningPosition + randomPosition, Quaternion.identity);
-    }
-
-    void SpawnLampInLine(Vector3 position, int direction)
-    {
-        if (direction == 0) // Spawn in x direction
-        {
-            position += new Vector3(10f, 0f, 0f);
-        }
-        else // Spawn in z direction
-        {
-            position += new Vector3(0f, 0f, 10f);
-        }
-        Instantiate(lampPrefab, spawningPosition + position, Quaternion.identity);
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            Vector3 linePosition = spawningPosition;
-            for (int i = 0; i < 10; i++)
-            {
-                if (i == 5)
-                {
-                    linePosition = new Vector3(spawningPosition.x, spawningPosition.y, spawningPosition.z + 12f); // Reset position for the next(to) line
-                }
-                SpawnLampInLine(linePosition, 0); // 0 == x
-                linePosition += new Vector3(10f, 0f, 0f);
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.B))
+            animator.SetTrigger("color");
+        } else if (Input.GetKeyDown(KeyCode.A))
         {
-            for (int i = 0; i < 5; i++)
-            {
-                SpawnLampRandom();
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.C))
+            animator.SetTrigger("scale");
+        } else if (Input.GetKeyDown(KeyCode.S))
         {
-            GameObject[] lamps = GameObject.FindGameObjectsWithTag("Lamp");
-            foreach (GameObject lamp in lamps)
-            {
-                Destroy(lamp);
-            }
+            animator.SetTrigger("rotate");
+        } else if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetTrigger("move");
         }
-    }
-
-    void OnGUI()
-    {
-        // Debug.Log("Check");
-        Rect instruction = new Rect(20, 20, 400, 80);
-        GUI.Box(instruction, "Press A to spawn lamps in a line \nB for random\nC for clear all lamps");
     }
 }
