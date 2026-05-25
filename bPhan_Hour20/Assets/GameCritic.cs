b0d8270049db89e915d9324ebd9b46bad92d1478
Phan Vũ Tuấn Anh <88922348+MarcoPhanVu@users.noreply.github.com>
2026-05-24 23:47:21 -0700

    H20 submit

---
+++ bPhan_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,16 @@
+/*
+The game is quite hard for me because I it's a bit too fast for me so I have to reduce the overall speed.
+
+I think we can add more assets or entities or powerups to the game to make it more appealling.
+
+Getting highscores isfun but also tedious because the game progress kind of linearly.
+
+If I have the time, I'll try to add more features like other endless runner games to make it more fun and dynamic.
+
+*/
+
+/* Changes that I made:
+Speed of the game.
+
+Overall Textures(replaced almost every thing)
+*/
