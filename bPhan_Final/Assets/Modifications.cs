65cdc171583edc8dce5197a6990317337e9b9587
Phan Vũ Tuấn Anh <88922348+MarcoPhanVu@users.noreply.github.com>
2026-05-31 23:50:21 -0700

    prepare Final

---
+++ bPhan_Final/Assets/Modifications.cs
@@ -0,0 +1,46 @@
+﻿public class Modifications
+{/*
+    ░▒▓████████▓▒░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ */
+
+
+    // Choose 1 of the games below and list modifications 
+    // Delete the other 2 games
+
+
+    /*  ================================================================
+        AMAZING RACER
+        ================================================================
+        1 + 3 - directional light and skybox changed (colour, intensity, direction,...)
+
+        2 - fog: I don't like to have fog hehe
+
+        4 Player: - I creaeted a simple worm object but I did add animation and a smol particle system to make it look more alive.
+
+        5 - use gameobject hierarchy to keep it organised: yes I did
+
+        FINISH ZONE
+        6 - convert to prefab, create 2 variants, and add an instance of each variant around the scene
+        modifications: added particle system to both so that the player can see where to go early
+
+        7 - modify the light component of the variants so that all have a different colour
+        [no record necessary]
+
+        8 - added 3-4 particle systems:
+        -All instances will update to include the particle system
+        -Don't forget to reassign any public fields on the instances to get them to work
+        parameter 1: Shape(size and rotation)
+        parameter 2: Emission(rate)
+        parameter 3: Color over lifetime
+
+        TERRAIN
+        9 - raise/lower to reshape and make it distinctly different from original
+        modifications: Added bounds to prevent player from falling off the map, and disguised them as mountains and hills.
+
+    */
+}
